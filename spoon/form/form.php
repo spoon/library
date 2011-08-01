0b1db4f8711bab6873619ab4ab3e99e29980e97f
Bauffman <davy@spoon-library.com>
2011-08-01 22:53:35 +0200

    SpoonForm::getTemplateExample has been cleaned up (in particular for checkboxes)

--- spoon/form/form.php
+++ spoon/form/form.php
@@ -776,8 +776,8 @@ class SpoonForm
 				elseif($object instanceof SpoonFormCheckbox)
 				{
 					$value .= "\t<p>\n";
-					$value .= "\t\t" . '<label for="' . $object->getAttribute('id') . '">' . SpoonFilter::toCamelCase($object->getName()) . "</label>\n";
 					$value .= "\t\t{\$chk" . SpoonFilter::toCamelCase($object->getName()) . "}\n";
+					$value .= "\t\t" . '<label for="' . $object->getAttribute('id') . '">' . SpoonFilter::toCamelCase($object->getName()) . "</label>\n";
 					$value .= "\t\t{\$chk" . SpoonFilter::toCamelCase($object->getName()) . "Error}\n";
 					$value .= "\t</p>\n";
 				}
