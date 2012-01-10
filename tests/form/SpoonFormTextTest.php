1e303fb01e378513eff70185e6afeceb05c1ba80
Bauffman <davy@spoon-library.com>
2012-01-10 10:39:04 +0100

    Updated tests to reflect FormText changes.

--- tests/form/SpoonFormTextTest.php
+++ tests/form/SpoonFormTextTest.php
@@ -52,185 +52,234 @@ class SpoonFormTextTest extends PHPUnit_Framework_TestCase
 
 	public function testIsFilled()
 	{
-		$this->assertEquals(false, $this->txtName->isFilled());
+		$this->assertFalse($this->txtName->isFilled());
 		$_POST['name'] = 'I am not empty';
-		$this->assertEquals(true, $this->txtName->isFilled());
+		$this->assertTrue($this->txtName->isFilled());
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertTrue($this->txtName->isFilled());
 	}
 
 	public function testIsAlphabetical()
 	{
-		$this->assertEquals(false, $this->txtName->isAlphabetical());
+		$this->assertFalse($this->txtName->isAlphabetical());
 		$_POST['name'] = 'Bauffman';
-		$this->assertEquals(true, $this->txtName->isAlphabetical());
+		$this->assertTrue($this->txtName->isAlphabetical());
+
+		// since the array will be casted to a string, this will be 'Array' and that is alphabetical :)
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertTrue($this->txtName->isAlphabetical());
 	}
 
 	public function testIsAphaNumeric()
 	{
 		$_POST['name'] = 'Spaces are not allowed?';
-		$this->assertEquals(false, $this->txtName->isAlphaNumeric());
+		$this->assertFalse($this->txtName->isAlphaNumeric());
 		$_POST['name'] = 'L33t';
-		$this->assertEquals(true, $this->txtName->isAlphaNumeric());
+		$this->assertTrue($this->txtName->isAlphaNumeric());
+
+		// since the array will be casted to a string, this will be 'Array' and that is alphabetical :)
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertTrue($this->txtName->isAlphaNumeric());
 	}
 
 	public function testIsBetween()
 	{
 		$_POST['name'] = '101';
-		$this->assertEquals(true, $this->txtName->isBetween(1, 102));
-		$this->assertEquals(true, $this->txtName->isBetween(-101, 101));
-		$this->assertEquals(false, $this->txtName->isBetween(200, 201));
-		$this->assertEquals(false, $this->txtName->isBetween(1000, 200));
+		$this->assertTrue($this->txtName->isBetween(1, 102));
+		$this->assertTrue($this->txtName->isBetween(-101, 101));
+		$this->assertFalse($this->txtName->isBetween(200, 201));
+		$this->assertFalse($this->txtName->isBetween(1000, 200));
+
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isBetween(10, 200));
 	}
 
 	public function testIsBool()
 	{
 		$_POST['name'] = 'true';
-		$this->assertEquals(true, $this->txtName->isBool());
+		$this->assertTrue($this->txtName->isBool());
 		$_POST['name'] = '1';
-		$this->assertEquals(true, $this->txtName->isBool());
+		$this->assertTrue($this->txtName->isBool());
 		$_POST['name'] = 'false';
-		$this->assertEquals(true, $this->txtName->isBool());
+		$this->assertTrue($this->txtName->isBool());
 		$_POST['name'] = '0';
-		$this->assertEquals(true, $this->txtName->isBool());
+		$this->assertTrue($this->txtName->isBool());
 		$_POST['name'] = 'I liek boobies';
-		$this->assertEquals(false, $this->txtName->isBool());
+		$this->assertFalse($this->txtName->isBool());
 		$_POST['name'] = '101';
-		$this->assertEquals(false, $this->txtName->isBool());
+		$this->assertFalse($this->txtName->isBool());
 		$_POST['name'] = '090';
-		$this->assertEquals(false, $this->txtName->isBool());
+		$this->assertFalse($this->txtName->isBool());
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isBool());
 	}
 
 	public function testIsDigital()
 	{
 		$_POST['name'] = '090';
-		$this->assertEquals(true, $this->txtName->isDigital());
+		$this->assertTrue($this->txtName->isDigital());
 		$_POST['name'] = 'Douchebag';
-		$this->assertEquals(false, $this->txtName->isDigital());
+		$this->assertFalse($this->txtName->isDigital());
 		$_POST['name'] = '';
-		$this->assertEquals(false, $this->txtName->isDigital());
+		$this->assertFalse($this->txtName->isDigital());
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isDigital());
 	}
 
 	public function testIsEmail()
 	{
-		$this->assertEquals(false, $this->txtName->isEmail());
+		$this->assertFalse($this->txtName->isEmail());
 		$_POST['name'] = 'davy@spoon-library.be';
-		$this->assertEquals(true, $this->txtName->isEmail());
+		$this->assertTrue($this->txtName->isEmail());
 		$_POST['name'] = '';
-		$this->assertEquals(false, $this->txtName->isEmail());
+		$this->assertFalse($this->txtName->isEmail());
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isEmail());
 	}
 
 	public function testIsFilename()
 	{
-		$this->assertEquals(false, $this->txtName->isFilename());
+		$this->assertFalse($this->txtName->isFilename());
 		$_POST['name'] = 'something.jpg';
-		$this->assertEquals(true, $this->txtName->isFilename());
+		$this->assertTrue($this->txtName->isFilename());
+
+		// since the array will be casted to a string, this will be 'Array' and that is a possible filename :)
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertTrue($this->txtName->isFilename());
 	}
 
 	public function testIsFloat()
 	{
-		$this->assertEquals(false, $this->txtName->isFloat());
+		$this->assertFalse($this->txtName->isFloat());
 		$_POST['name'] = 1.1;
-		$this->assertEquals(true, $this->txtName->isFloat());
+		$this->assertTrue($this->txtName->isFloat());
 		$_POST['name'] = -209;
-		$this->assertEquals(true, $this->txtName->isFloat());
+		$this->assertTrue($this->txtName->isFloat());
 		$_POST['name'] = 199;
-		$this->assertEquals(true, $this->txtName->isFloat());
+		$this->assertTrue($this->txtName->isFloat());
 		$_POST['name'] = '1,35';
 		$this->assertTrue($this->txtName->isFloat(null, true));
 		$_POST['name'] = '-1,35';
 		$this->assertTrue($this->txtName->isFloat(null, true));
+
+		$_POST['name'] = array('foo', 'bar', 190);
+		$this->assertFalse($this->txtName->isFloat());
 	}
 
 	public function testIsGreatherThan()
 	{
 		$_POST['name'] = 199;
-		$this->assertEquals(true, $this->txtName->isGreaterThan(1));
-		$this->assertEquals(true, $this->txtName->isGreaterThan(-199));
-		$this->assertEquals(false, $this->txtName->isGreaterThan(199));
+		$this->assertTrue($this->txtName->isGreaterThan(1));
+		$this->assertTrue($this->txtName->isGreaterThan(-199));
+		$this->assertFalse($this->txtName->isGreaterThan(199));
+
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isGreaterThan(1337));
 	}
 
 	public function testIsInteger()
 	{
 		$_POST['name'] = 199;
-		$this->assertEquals(true, $this->txtName->isInteger());
+		$this->assertTrue($this->txtName->isInteger());
 		$_POST['name'] = -199;
-		$this->assertEquals(true, $this->txtName->isInteger());
+		$this->assertTrue($this->txtName->isInteger());
 		$_POST['name'] = 1.1;
-		$this->assertEquals(false, $this->txtName->isInteger());
+		$this->assertFalse($this->txtName->isInteger());
 		$_POST['name'] = '1,9';
-		$this->assertEquals(false, $this->txtName->isInteger());
+		$this->assertFalse($this->txtName->isInteger());
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isInteger());
 	}
 
 	public function testIsIp()
 	{
-		$this->assertEquals(false, $this->txtName->isIp());
+		$this->assertFalse($this->txtName->isIp());
 		$_POST['name'] = '127.0.0.1';
-		$this->assertEquals(true, $this->txtName->isIp());
+		$this->assertTrue($this->txtName->isIp());
 		$_POST['name'] = '192.168.1.101';
-		$this->assertEquals(true, $this->txtName->isIp());
+		$this->assertTrue($this->txtName->isIp());
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isIp());
 	}
 
 	public function testIsMaximum()
 	{
 		$_POST['name'] = 'Spanks';
-		$this->assertEquals(true, $this->txtName->isMaximum(10));
+		$this->assertTrue($this->txtName->isMaximum(10));
 		$_POST['name'] = 199;
-		$this->assertEquals(false, $this->txtName->isMaximum(18));
-		$this->assertEquals(true, $this->txtName->isMaximum(300));
+		$this->assertFalse($this->txtName->isMaximum(18));
+		$this->assertTrue($this->txtName->isMaximum(300));
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertTrue($this->txtName->isMaximum(200));
 	}
 
 	public function testIsMaximumCharacters()
 	{
 		$_POST['name'] = 'Writing tests can be pretty frakkin boring';
-		$this->assertEquals(true, $this->txtName->isMaximumCharacters(100));
-		$this->assertEquals(false, $this->txtName->isMaximumCharacters(10));
+		$this->assertTrue($this->txtName->isMaximumCharacters(100));
+		$this->assertFalse($this->txtName->isMaximumCharacters(10));
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertTrue($this->txtName->isMaximumCharacters(10));
 	}
 
 	public function testIsMinimum()
 	{
 		$_POST['name'] = 5;
-		$this->assertEquals(true, $this->txtName->isMinimum(5));
-		$this->assertEquals(true, $this->txtName->isMinimum(4));
-		$this->assertEquals(false, $this->txtName->isMinimum(7));
+		$this->assertTrue($this->txtName->isMinimum(5));
+		$this->assertTrue($this->txtName->isMinimum(4));
+		$this->assertFalse($this->txtName->isMinimum(7));
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isMinimum(7));
 	}
 
 	public function testIsMinimumCharacters()
 	{
 		$_POST['name'] = 'Stil pretty bored';
-		$this->assertEquals(true, $this->txtName->isMinimumCharacters(10));
-		$this->assertEquals(true, $this->txtName->isMinimumCharacters(2));
-		$this->assertEquals(false, $this->txtName->isMinimumCharacters(23));
+		$this->assertTrue($this->txtName->isMinimumCharacters(10));
+		$this->assertTrue($this->txtName->isMinimumCharacters(2));
+		$this->assertFalse($this->txtName->isMinimumCharacters(23));
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isMinimumCharacters(23));
 	}
 
 	public function testIsNumeric()
 	{
 		$_POST['name'] = '010192029';
-		$this->assertEquals(true, $this->txtName->isNumeric());
+		$this->assertTrue($this->txtName->isNumeric());
 		$_POST['name'] = '1337';
-		$this->assertEquals(true, $this->txtName->isNumeric());
+		$this->assertTrue($this->txtName->isNumeric());
 		$_POST['name'] = 'I can haz two cheezeburgers?';
-		$this->assertEquals(false, $this->txtName->isNumeric());
+		$this->assertFalse($this->txtName->isNumeric());
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isNumeric());
 	}
 
 	public function testIsSmallerThan()
 	{
 		$_POST['name'] = 137;
-		$this->assertEquals(true, $this->txtName->isSmallerThan(138));
-		$this->assertEquals(true, $this->txtName->isSmallerThan(200));
-		$this->assertEquals(false, $this->txtName->isSmallerThan(0));
-		$this->assertEquals(false, $this->txtName->isSmallerThan(-16));
+		$this->assertTrue($this->txtName->isSmallerThan(138));
+		$this->assertTrue($this->txtName->isSmallerThan(200));
+		$this->assertFalse($this->txtName->isSmallerThan(0));
+		$this->assertFalse($this->txtName->isSmallerThan(-16));
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isSmallerThan(0));
 	}
 
 	public function testIsURL()
 	{
 		$_POST['name'] = 'http://www.spoon-library.com';
-		$this->assertEquals(true, $this->txtName->isURL());
+		$this->assertTrue($this->txtName->isURL());
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertFalse($this->txtName->isURL());
 	}
 
 	public function testIsValidAgainstRegexp()
 	{
 		$_POST['name'] = 'Spoon';
-		$this->assertEquals(true, $this->txtName->isValidAgainstRegexp('/([a-z]+)/'));
-		$this->assertEquals(false, $this->txtName->isValidAgainstRegexp('/([0-9]+)/'));
+		$this->assertTrue($this->txtName->isValidAgainstRegexp('/([a-z]+)/'));
+		$this->assertFalse($this->txtName->isValidAgainstRegexp('/([0-9]+)/'));
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertTrue($this->txtName->isValidAgainstRegexp('/Array/'));
 	}
 
 	public function testGetValue()
@@ -239,5 +288,7 @@ class SpoonFormTextTest extends PHPUnit_Framework_TestCase
 		$_POST['name'] = '<a href="http://www.spoon-library.be">Bobby Tables, my friends call mééé</a>';
 		$this->assertEquals(SpoonFilter::htmlspecialchars($_POST['name']), $this->txtName->getValue());
 		$this->assertEquals($_POST['name'], $this->txtName->getValue(true));
+		$_POST['name'] = array('foo', 'bar');
+		$this->assertEquals('Array', $this->txtName->getValue());
 	}
 }
