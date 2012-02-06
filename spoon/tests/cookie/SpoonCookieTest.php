04e80fa856408eed19b6081be3cbb6b622e99a59
Bauffman <davy@spoon-library.com>
2012-02-06 10:44:29 +0100

    Fixes include path.

--- spoon/tests/cookie/SpoonCookieTest.php
+++ spoon/tests/cookie/SpoonCookieTest.php
@@ -1,5 +1,8 @@
 <?php
 
+$includePath = dirname(dirname(dirname(dirname(__FILE__))));
+set_include_path(get_include_path() . PATH_SEPARATOR . $includePath);
+
 require_once 'spoon/spoon.php';
 require_once 'PHPUnit/Framework/TestCase.php';
 
