b848f91e7d62c59e3e62d13f39ff8ac85d600545
Bauffman <davy@spoon-library.com>
2012-02-06 10:38:56 +0100

    Cleans up SpoonCookieTest.

--- spoon/tests/cookie/spoon_cookie_test.php
+++ spoon/tests/cookie/spoon_cookie_test.php
@@ -1,18 +1,10 @@
 <?php
 
-// includes
 require_once 'spoon/spoon.php';
 require_once 'PHPUnit/Framework/TestCase.php';
 
 class SpoonCookieTest extends PHPUnit_Framework_TestCase
 {
-	public function testDelete()
-	{
-		SpoonCookie::set('key', 'value');
-		SpoonCookie::delete('key');
-		$this->assertFalse(false, SpoonCookie::exists('key'));
-	}
-
 	public function testExists()
 	{
 		$this->assertFalse(SpoonCookie::exists('honka_honka'));
@@ -22,11 +14,4 @@ class SpoonCookieTest extends PHPUnit_Framework_TestCase
 	{
 		$this->assertFalse(SpoonCookie::get('why_does_my_neck_hurt'));
 	}
-
-	public function testSet()
-	{
-		$this->assertTrue(SpoonCookie::set('name', 'Spoon Library'));
-	}
 }
-
-?>
