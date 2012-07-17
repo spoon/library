886b6946a6a799fc6085d9b0d8012e47394bdcfc
Siesqo <jeroen@siesqo.be>
2012-07-17 09:03:44 +0200

    Locale: fixed 2 typos

--- spoon/locale/data/continent_countries.php
+++ spoon/locale/data/continent_countries.php
@@ -6,7 +6,7 @@ $continentCountries = array();
 // countries in Africa
 $continentCountries['africa'] = array('DZ', 'AO', 'BJ', 'BW', 'BF', 'BI', 'CM', 'CV', 'CF', 'TD', 'KM', 'CG', 'CD', 'DJ', 'GQ', 'ER', 'ET', 'GA', 'GM', 'GH', 'GN', 'GW', 'CI', 'KE', 'LS', 'LR', 'LY', 'MG', 'MW', 'ML', 'MR', 'MU', 'YT', 'MA', 'MZ', 'NA', 'NE', 'NG', 'RE', 'RW', 'SH', 'ST', 'SN', 'SC', 'SL', 'SO', 'ZA', 'LK', 'SZ', 'TZ', 'TG', 'TN', 'UG', 'EH', 'ZM', 'ZM');
 
-// countries in Antarcita
+// countries in Antarctica
 $continentCountries['antarctica'] = array('AQ', 'BV', 'TF', 'HM', 'GS');
 
 // countries in Asia
@@ -15,7 +15,7 @@ $continentCountries['asia'] = array('AF', 'AM', 'AZ', 'BH', 'BD', 'BT', 'IO', 'B
 // countries in Europe
 $continentCountries['europe'] = array('AX', 'AL', 'AD', 'AT', 'BY', 'BE', 'BA', 'BG', 'HR', 'CY', 'EE', 'QU', 'FO', 'FI', 'FR', 'DE', 'GI', 'GR', 'GG', 'HU', 'IS', 'IE', 'IM', 'IT', 'JE', 'LV', 'LI', 'LT', 'LU', 'MK', 'MT', 'MD', 'MC', 'ME', 'NL', 'NO', 'PL', 'PT', 'RO', 'RU', 'SM', 'RS', 'RS', 'SK', 'SI', 'ES', 'SJ', 'SE', 'CH', 'UA', 'GB', 'VA');
 
-// countries in Nord America
+// countries in North America
 $continentCountries['nordAmerica'] = array('AI', 'AG', 'AW', 'BS', 'BB', 'BZ', 'BM', 'VG', 'CA', 'KY', 'CR', 'CU', 'DM', 'DO', 'SV', 'GD', 'GP', 'GT', 'HT', 'HN', 'JM', 'MQ', 'MX', 'MS', 'AN', 'NI', 'PA', 'PR', 'BL', 'KN', 'LC', 'MF', 'PM', 'VC', 'TT', 'TC', 'VI', 'US', 'UM');
 
 // countries in Oceania
